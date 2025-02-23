c6ebbf62f41cef0d4007e3ca05c2dd5103bd224d
savkin <godbryansk@yandex.ru>
2025-02-23 22:24:18 +0300

    ничего нового

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+_filter = {'<?xml version="1.0" encoding="UTF-8"?>', '<NaturalPerson>', '<ListID>', '<ListTime>', '<ListItem>',
+           '<Document>', '<DocumentID>', '<Procedure>', '<NaturalPerson>', '<Surname>'}
 # в списке хранится номер элемента списка с тегом <Applicant type=
 xml_epgu_elem = []
 # в списке каждое лицо хранится в отдельном списке
 xml_epgu = []
-
 
 def open_and_strip_xml() -> list[str]:
     """
@@ -21,7 +22,7 @@ def open_and_strip_xml() -> list[str]:
     return _xml_after_strip
 
 
-def check_xml(bind_xml_after_strip: list[str]) -> list[str]:
+def check_and_match_xml(bind_xml_after_strip: list[str]) -> list[str]:
     """
     Переименовать, не только проверка, но и урезание. Возмножно переработать, с учетом блока <ConvictionPerson>? (внизу)
     :param bind_xml_after_strip:
@@ -31,25 +32,41 @@ def check_xml(bind_xml_after_strip: list[str]) -> list[str]:
         case ['<?xml version="1.0" encoding="UTF-8"?>', '<List>', _, _, _,
               '<Document>', _,
               '<Procedure>issue</Procedure>',
-              *_xml_after_match, '</Document>',
+              *_xml_after_check_and_match, '</Document>',
               '</List>']:
-            print("xml_after_match: ", _xml_after_match)
+            print("xml_after_check_and_match: ", _xml_after_check_and_match)
         case _:
             raise SyntaxError("Формат файла XML не стандартный")
-    return _xml_after_match
+    return _xml_after_check_and_match
+
+
+def cut_xml(bind_xml_after_check_and_match: list[str]) -> list[str]:
+    """НЕ РАБОТАЕТ - НУЖНО УБРАТЬ ВСЕ ЛИШНИЕ ТЕГИ"""
+    _xml_after_cut = []
+
+    for el in _filter:
+        for line in xml_after_check_and_match:
+            if el in line:
+                pass
+            else:
+                _xml_after_cut.append(line)
+
+    return _xml_after_cut
 
 
 if __name__ == '__main__':
 
-    xml_after_strip: list[str] = open_and_strip_xml()
-    print("xml_after_strip: ", xml_after_strip)
+    xml_after_open_and_strip: list[str] = open_and_strip_xml()
+    print("xml_after_open_and_strip: ", xml_after_open_and_strip)
 
     # список для обработки xml-файла - удаления из него всё до тега <Document> и проверка его на небольшое соответствие
-    xml_after_match: list[str] = check_xml(xml_after_strip)
+    xml_after_check_and_match: list[str] = check_and_match_xml(xml_after_open_and_strip)
 
+    xml_after_cut : list[str] = cut_xml(xml_after_check_and_match)
+    print('xml_after_cut: ', xml_after_cut)
 
     # Находим все вхождения новых лиц по типам подачи заявлений (тэг <Applicant type=)
-    for index, el in enumerate(xml_after_match):
+    for index, el in enumerate(xml_after_check_and_match):
         if '<Applicant type=' in el:
             xml_epgu_elem.append(index)
             # print(xml_after_match[index:])
@@ -61,8 +78,8 @@ if __name__ == '__main__':
 
     # Добавляем в список отдельно списки по лицам
     for el in xml_epgu_elem:
-        xml_epgu.append(xml_after_match[el:])
-        del xml_after_match[el:]
+        xml_epgu.append(xml_after_check_and_match[el:])
+        del xml_after_check_and_match[el:]
 
     xml_epgu = xml_epgu[::-1]
 
