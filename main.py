66554704c60c27b90acc074b83c4a9f4cd1239b3
savkin <godbryansk@yandex.ru>
2025-02-27 21:28:59 +0300

    rename xml_tags, modify cut_xml, check_xml

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tag_filter import tag_filter
+from xml_tags import tags
 
 # в списке хранится номер элемента списка с тегом <Applicant type=
 xml_epgu_elem = []
@@ -14,7 +14,7 @@ def open_and_strip_xml():
     # список для обработки исходного xml-файла (без пробелов слева и справа)
     _xml_after_strip = []
     # открываем xml-файл в формате utf-8
-    with open("Запросы от 20250113155643.xml", "r", encoding="utf-8") as xml_input:
+    with open('Запросы от 20250113155643.xml', 'r', encoding='utf-8') as xml_input:
         # читаем xml-файл построчно
         for line in xml_input:
             # берем каждую строку исходного xml-файла и удаляем лишние пробелы слева и справа
@@ -31,25 +31,22 @@ def check_xml(bind_xml_after_open):
     match bind_xml_after_open:
         case ['<?xml version="1.0" encoding="UTF-8"?>', '<List>', _, _, _, '<Document>', _,
               '<Procedure>issue</Procedure>', *_, '</Document>', '</List>']:
-            print("Стандартный XML")
+            print("Проверка XML: Стандартный!")
         case _:
             raise SyntaxError("Ошибка. Формат файла XML не стандартный")
     return 0
 
-def cut_xml(bind_xml_after_strip, bind_tag_filter):
+def cut_xml(bind_xml_after_strip, bind_tags):
     """
-    Обрезаем XML-файл по фильтру
+    Сокращает XML-файл, сопоставляя исходный XML со списком тегов, которые должны остаться
     :param bind_xml_after_strip:
-    :param bind_tag_filter:
-    :return: Списковое включение
+    :param bind_tags:
+    :return: список лиц с Видом заявления, ФИО и Датой рождения
     """
-    #return [item for item in bind_xml_after_strip if item not in bind_tag_filter]
-    return [tag
-                for tag in bind_xml_after_strip
-                    for flt in bind_tag_filter
-                        if tag.startswith(flt)
-            ]
-    # ПЕРЕДАЛАТЬ ФИЛЬТР ПО НУДНЫМ ТЕГАМ, А НЕ ПО НЕНУЖНЫМ,,,,,,,!!!
+    return [el
+            for el in bind_xml_after_strip
+                for tag in bind_tags
+                    if el.startswith(tag)]
 
 
 if __name__ == '__main__':
@@ -57,17 +54,17 @@ if __name__ == '__main__':
     xml_after_open_and_strip = open_and_strip_xml()
     print("xml_after_open_and_strip: ", xml_after_open_and_strip)
 
+    # Проверка XML-файла
+    check_xml(xml_after_open_and_strip)
+
     # Обрезаем XML-файл
-    xml_cut = cut_xml(xml_after_open_and_strip, tag_filter)
+    xml_cut = cut_xml(xml_after_open_and_strip, tags)
     print('xml_cut: ', xml_cut)
 
     # Находим все вхождения новых лиц по типам подачи заявлений (тэг <Applicant type=)
     for index, el in enumerate(xml_cut):
         if '<Applicant type=' in el:
             xml_epgu_elem.append(index)
-            # print(xml_after_match[index:])
-        # if el == '<ConvictionPerson>':
-        #    print(1)
 
     xml_epgu_elem = xml_epgu_elem[::-1]
     print(xml_epgu_elem)
@@ -78,28 +75,7 @@ if __name__ == '__main__':
         del xml_cut[el:]
 
     xml_epgu = xml_epgu[::-1]
-"""
 
-    # получаем список с лицом, из общего списка всех лиц
-    for el_list in xml_epgu:
-        # перебираем все теги
-        for el in el_list:
-            if '<CPSurname>' in el:
-                print(el, end='')
-            if '<CPName>' in el:
-                print(el, end='')
-            if '<CPPatronymic>' in el:
-                print(el, end='')
-            if '<CPBirthday>' in el:
-                print(el)
-            # Продумать блок со старыми ФИО???
-            if '<CPLastFIO>' in el:
-                print('(', end='')
-            if '<CPLSurname>' in el:
-                print(el + ')')
 
     for el in xml_epgu:
         print(el)
-"""
-    # попробовать match/case уменьшить список и оставить блок <ConvictionPerson>?
-    # ИЛИ лучше оставить ТЕГ <Applicant type=" и блок <ConvictionPerson>?
