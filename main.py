3834e719fd5de6f6d5e77048e813a6d0251db2d8
savkin <godbryansk@yandex.ru>
2025-02-28 21:37:10 +0300

    naming def decomp...

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def cut_xml(bind_xml_after_strip, bind_tags):
                 for tag in bind_tags
                     if el.startswith(tag)]
 
-def index_xml(bind_xml_cut):
+def index_xml(bind_xml_cut) -> list[int]:
     """
     По обрезанному XML-файлу находим тег <Applicant type=> и в отдельном списке проставляем по этому тегу индексы.
     Инверсируем этот список.
@@ -52,22 +52,23 @@ def index_xml(bind_xml_cut):
     """
     return [index for index, tag in enumerate(bind_xml_cut) if tag.startswith('<Applicant type=')][::-1]
 
-def build_list_by_tag_from_xml(bind_xml_after_cut, bind_xml_after_index):
+def decomposing_xml_by_indexes_into_lists(bind_xml_after_cut, bind_xml_after_index):
     """
-    Создает список списков по списку индексов, отобранных в функции index_xml, через аргумент bind_xml_after_index.
+    Создает список списков по списку индексов, полученных их функции index_xml, через аргумент bind_xml_after_index.
     В каждый отдельный список помещается уникальное лицо.
     :param bind_xml_after_cut:
     :param bind_xml_after_index:
     :return: Список списков с отдельными лицами.
     """
-    xml_epgu = []
+    # xml после разбивки по индексам на списки
+    _xml_after_decomposing_by_indexes_into_lists = []
     # Добавляем в список отдельно списки по лицам
     for el in bind_xml_after_index:
-        xml_epgu.append(bind_xml_after_cut[el:])
+        _xml_after_decomposing_by_indexes_into_lists.append(bind_xml_after_cut[el:])
         del bind_xml_after_cut[el:]
 
-    xml_epgu = xml_epgu[::-1]
-    return xml_epgu
+    _xml_after_decomposing_by_indexes_into_lists = _xml_after_decomposing_by_indexes_into_lists[::-1]
+    return _xml_after_decomposing_by_indexes_into_lists
 
 
 if __name__ == '__main__':
@@ -75,7 +76,7 @@ if __name__ == '__main__':
     xml_after_open_and_strip = open_and_strip_xml()
     print("xml_after_open_and_strip: ", xml_after_open_and_strip)
 
-    # Проверяем XML-файла
+    # Проверяем XML-файл
     check_xml(xml_after_open_and_strip)
 
     # Обрезаем XML-файл
@@ -83,12 +84,13 @@ if __name__ == '__main__':
     print('xml_cut: ', xml_after_cut)
 
     # Создаем список с индексами по обрезанному XML-файл по тегу <Applicant type=>
-    xml_after_index = index_xml(xml_after_cut)
+    xml_after_index: list[int] = index_xml(xml_after_cut)
     print('xml_after_index: ', xml_after_index)
 
-    xml_epgu = build_list_by_tag_from_xml(xml_after_cut, xml_after_index)
+    # Создаем список списков, в каждом из которых уникальные лица со всем тегами, отфильтрованными до этого этапа
+    xml_after_decomposing_by_indexes_into_lists = decomposing_xml_by_indexes_into_lists(xml_after_cut, xml_after_index)
 
-    for el in xml_epgu:
+    for el in xml_after_decomposing_by_indexes_into_lists:
         print(el)
 
-    print(xml_after_cut)
+    #print('\n', '* ' * 50, '\n', 'Список [xml_after_cut] должен стать пустым: (НУЖЕН ТЕСТ?)', xml_after_cut, '\n', '* ' * 50)
